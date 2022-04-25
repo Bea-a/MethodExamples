fb379e1b479b03da3bc2a0bcd78db3d6950f8ba9
Bea Vitkaus <61853134+Bea-a@users.noreply.github.com>
2022-04-25 17:16:05 +0100

    Add files via upload
    IF statement with tidied up code

---
+++ Decisions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bea's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = "";
+
+            if (userValue == "1")
+            {
+                message = "You won a new car!";
+                
+            }
+            else if (userValue == "2")
+            {   
+                message = ("Sorry you didn't win anything this time");
+               
+            }
+            else if(userValue == "3")
+            {
+                message = ("You won a pet of your choice!");
+                
+            }
+            else
+            {
+                message = "Sorry, we didnt't understand.";
+                
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
